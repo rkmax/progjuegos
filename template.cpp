bba6b4dc910ac7b609b2aed8365767bf1c48e567
drn <drion1929@aim.com>
2012-02-10 17:04:07 +0100

    Plantilla

---
+++ template.cpp
@@ -0,0 +1,25 @@
+/* ****************************
+ * ****************************
+ * **       TEMPLATE         **
+ * ****************************
+ * ****************************
+ * ---------------------------------------------------------------------
+ *							::ProgJuegos::
+ *  Reporitory:
+ * 			git:	git://gitorious.org/progjuegos/progjuegos.git
+ * 			ssh:	git@gitorious.org:progjuegos/progjuegos.git
+ * 			http:	https://git.gitorious.org/progjuegos/progjuegos.git
+ * 
+ *	Mail:	progjuegos@googlegroups.com
+ * 
+ * ---------------------------------------------------------------------
+ * 
+ * */
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+return 0;
+}
+
